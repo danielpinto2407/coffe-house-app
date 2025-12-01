dd743232c21168a002e1d61260b9221ce2e465b6
Daniel <wdpinto@utp.edu.co>
2025-12-01 11:29:28 -0500

    (feature): Commit inicial con estructura del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'menu',
+    loadChildren: () =>
+      import('./features/menu/menu.routes').then(r => r.MENU_ROUTES),
+  },
+  { path: '', redirectTo: 'menu', pathMatch: 'full' },
+];
---
+++ src/app/features/menu/pages/menu.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu.page.html',
+  styleUrl: './menu.page.css'
+})
+export class MenuPage {
+
+}
